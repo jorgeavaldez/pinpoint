b3d7743bf0255a1a47dc6f1e6a077d99739e32ed
Jorge Valdez <jorge@yung.dev>
2025-03-24 09:24:47 -0400

    format and add tanstack query

--- apps/frontend/app/routes/home.tsx
+++ apps/frontend/app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Pinpoint" },
-    { name: "description", content: "store it now!" },
-  ];
+export function meta() {
+	return [
+		{ title: "Pinpoint" },
+		{ name: "description", content: "store it now!" },
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <Welcome />;
 }
