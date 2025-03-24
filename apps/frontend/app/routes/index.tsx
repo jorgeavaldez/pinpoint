49260cc3a9a424c2cf5e243b9ad54a7a254a85f1
Jorge Valdez <jorge@yung.dev>
2025-03-24 11:34:22 -0400

    add searchbox lib

--- apps/frontend/app/routes/index.tsx
+++ apps/frontend/app/routes/index.tsx
@@ -1,20 +1,32 @@
+import { SearchBoxCore } from "@mapbox/search-js-core";
 import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
-const getCount = createServerFn({ method: "GET" }).handler(() => {
-	return { count: 1 };
-});
+type SearchInput = {
+	lat: number;
+	lon: number;
+};
+
+const getLocationPoints = createServerFn({ method: "GET" })
+	.validator((location: SearchInput) => location)
+	.handler(async (ctx) => {
+		const searchbox = new SearchBoxCore({
+			accessToken:
+				"pk.eyJ1IjoiZGVsdmF6ZSIsImEiOiJjbThuMWtucGgxa2YzMmxweHZrNjZuNm1sIn0.2gjk3rH2em5ha1p8Gzb5nw",
+		});
+		const res = await searchbox.reverse(ctx.data)
+		console.dir(res)
+		return res;
+	});
 
 export const Route = createFileRoute("/")({
 	component: Home,
-	loader: async () => await getCount(),
 });
 
 function Home() {
-	const state = Route.useLoaderData();
 	const [coords, setCoords] = useState<GeolocationPosition | null>(null);
 
 	const { mutate, isPending } = useMutation({
@@ -29,7 +41,6 @@ function Home() {
 	return (
 		<main>
 			<h1>Home</h1>
-			<p>Count: {state.count}</p>
 			<Button onClick={() => mutate()} disabled={isPending}>
 				{isPending ? "Locating..." : "Get My Location"}
 			</Button>
